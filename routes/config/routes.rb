e6ee852b7a86981048500eb1c6ea9c853b7861d2
root <root@DESKTOP-903EI4M.localdomain>
2023-03-08 14:56:31 -0800

    phase 1

---
+++ routes/config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # resources :users
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get '/users', to: 'users#index', as: 'users'
+  post '/users', to: 'users#create'
+
+  get '/users/:id', to: 'users#show', as: 'user'
+  patch '/users/:id', to: 'users#update'
+  put '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+
+end

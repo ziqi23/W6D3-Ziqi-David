9360e094afe31e479f12180a1ed263c8c69daf49
root <root@DESKTOP-903EI4M.localdomain>
2023-03-08 15:46:44 -0800

    phase 3

--- routes/app/controllers/users_controller.rb
+++ routes/app/controllers/users_controller.rb
@@ -2,4 +2,23 @@
 
 class UsersController < ApplicationController
 
+    def index
+        render plain: "I'm in the index action!"
+    end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
+
+    def update
+
+    end
+
+    def destroy
+        
+    end
 end
